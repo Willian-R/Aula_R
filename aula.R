5bc8000d5def99a4eb7cea9ab44f598f84a8c676
Willian B. Ribeiro <64652475+Willian-R@users.noreply.github.com>
2021-08-27 19:31:03 -0300

    Rede neural do minicurso

---
+++ aula.R
@@ -0,0 +1,33 @@
+##ajustar local de trabalho
+getwd()
+setwd("C:/Users/Casa/Documents/minicurso")
+
+##importar dados
+install.packages("datasets")
+require(datasets)
+library(datasets)
+
+?infert
+dados <- infert
+str(dados)
+names(dados)
+dim(dados)
+
+##partição dos meus dados
+##70 % treino, 30% teste
+particionar <- sample(2, nrow(dados), replace = TRUE, prob = c(0.7, 0.3))
+treino <- dados[particionar==1,]
+teste <- dados[particionar==2,]
+
+## rede neural artificial
+install.packages("neuralnet")
+library(neuralnet)
+
+rede <- neuralnet(case~age+parity+spontaneous+induced,
+                  hidden = 5,
+                  data = treino,
+                  rep = 10,
+                  err.fct = "ce", ##ce = cross-entropy (classificação); sse = soma do erro quadrado (regressão)
+                  lifesign = "minimal",
+                  linear.output = FALSE,
+                  stepmax = 1e6)
